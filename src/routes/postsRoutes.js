859f71dd01f00ecbcab79a1042086a4c79b870b6
jaovitim <162746128+jaovitim590@users.noreply.github.com>
2025-09-04 18:30:55 -0300

    Update postsRoutes.js

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,30 +1,10 @@
-import express from "express";
-import multer from "multer";
-
-import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost} from "../controllers/postsController.js";
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-})
-
-
-const upload = multer({dest:"./uploads", storage})
-
 const routes = (app) => {
     app.use(express.json());
 
-    app.use(cors(corsOptions))
-
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost);
+};
 
-    app.post("/posts", postarNovoPost)
-
-    app.post("/upload", upload.single("imagem"), uploadImagem)
-    
-    app.put("/upload/:id", atualizarNovoPost )
-}
 export default routes;
