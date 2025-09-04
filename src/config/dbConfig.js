b1c5a121643b1919ef54e02dca0c9c9ca426c87e
jaovitim <162746128+jaovitim590@users.noreply.github.com>
2025-09-04 17:15:23 -0300

    Update dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,22 +1,23 @@
 import { MongoClient } from 'mongodb';
 
-export default async function conectarAoBanco(STRING_CONEXAO) {
+export default async function conectarAoBanco() {
   let mongoClient;
 
   try {
     console.log('Conectando ao cluster do banco de dados...');
-    
-    // Atualizado: Removendo opções descontinuadas
+
+    const STRING_CONEXAO = process.env.STRING_CONEXAO; // pega a variável do Railway
+
     mongoClient = new MongoClient(STRING_CONEXAO, {
-      tls: true,  // Garantir uso de TLS
+      tls: true,
     });
 
-    await mongoClient.connect();  // Conectar ao banco de dados
+    await mongoClient.connect();
 
     console.log('Conectado ao MongoDB Atlas com sucesso!');
     return mongoClient;
   } catch (erro) {
     console.error('Falha na conexão com o banco!', erro);
-    process.exit(1);  // Forçar a saída do processo em caso de erro
+    process.exit(1);
   }
 }
